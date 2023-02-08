ca7d5b60114aea192f6e7aebeaa8252808c9f760
ASelvanth <ajselva04@gmail.com>
2023-02-08 11:05:25 +0530

    implement the chat component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-
+import './App.css';
 import { useState } from "react";
+import io from "socket.io-client";
 import {Routes , Route} from "react-router-dom";
 import UserContext from "./Context/UserContext";
 import useFindUser from "./Hooks/useFindUser";
-import Register from "./components/Register";
-import Chat from "./components/Chat";
+
 import Login from "./components/Login";
+import Register from "./components/Register";
+import JoinChat from "./components/JoinChat";
+
 import PublicRoutes from "./Routes/PublicRoutes";
+import PrivateRoutes from "./Routes/PrivateRoutes";
+
+const socket = io.connect('http://localhost:4000');
 
 function App() {
-  
-const [user, setUser] = useFindUser();
+  const [user, setUser , loading] = useFindUser();
+  console.log("User", user);
+    
 
   return (
     //user comes from custom hooks.. 
-    <UserContext.Provider value={{user}}>  
+    <UserContext.Provider value={{user , setUser , loading}}>  
     {/* //user- from context */}
       <div className="App">
         <Routes>
          <Route element={<PublicRoutes />}>
+            <Route path='/' element={<JoinChat socket={socket} /> } />
             <Route path='/login' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>          
           </Route> 
-          <Route path='/chat' element={<Chat/>}/>          
+                    
         </Routes>              
-      
+        <Routes element={<PrivateRoutes/>}>
+          {/* <Route path='/chat' element={<Chat/>}/> */}
+        </Routes>
       </div>
     </UserContext.Provider>
   );
--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,9 +1,64 @@
-import useFindUser from "../../Hooks/useFindUser";
+import { useEffect, useState } from "react";
 
-const Chat = () =>{
-    
+const Chat = ({socket , userName , room}) =>{
+    const [messages, setMessages] =useState([]);
+    const [currentMessage, setCurrentMessage] = useState("");
+
+    useEffect(()=>{
+        socket.on('receive-message', data =>{
+            setMessages(list => [...list , data])
+        })
+    },[socket])
+     const sendMessage =  async ()=>{
+        if(currentMessage){
+            const messageData = {
+                room : room,
+                author : userName,
+                message : currentMessage ,
+                time : new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes()
+
+            }
+            //sent data here 
+            await socket.emit('send-message', messageData);
+            setMessages((list) => [...list, messageData]);
+            setCurrentMessage("");
+        }
+    }
+
     return (
-        <div> Chat App </div>
+        <div>
+            <div className="chat-window">
+                <div className="chat-header">
+                    <h3>Live Chat</h3>
+                </div>
+                <div className="chat-body">
+                    <div className="message-container">
+                        {
+                        messages.map((messageContent) => (
+                                <div className="message" id="you">
+                                    <div className="message-content">
+                                        {messageContent.message}
+                                    </div>
+                                    <div className="message-meta">
+                                        <p id='time'>{messageContent.time}</p>
+                                        <p id='author'>{messageContent.author}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="chat-footer">
+                        <input
+                            type='text'
+                            value={currentMessage}
+                            onChange={e => setCurrentMessage(e.target.value)}
+                            onKeyPress={e => e.key === 'Enter' && sendMessage()}
+                        />
+                        <button onClick={sendMessage}>Send</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
